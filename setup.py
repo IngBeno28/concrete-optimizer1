a11795e3fcd02e369341f08bd674bada7f495ce8
IngBeno28 <wiafe1713@gmail.com>
2025-05-28 05:45:37 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="concrete_mix_optimizer",
+    version="0.1",
+    install_requires=[
+        "streamlit==1.32.0",
+        "pandas==2.1.4",
+        "matplotlib==3.8.2",
+        "fpdf2==2.7.7",
+    ],
+)
